245a4077ff02934dbf5fa472a5a9181d3cf10314
ThePatman3 <patrick.vanderbend@student.hu.nl>
2018-11-22 11:49:15 +0100

    Update PauseDetectionControl.hpp
    PauseDetectionControl now uses hwlib::now_us() to measure how long a pause was.

--- lib/PauseDetectionControl.hpp
+++ lib/PauseDetectionControl.hpp
@@ -15,6 +15,7 @@ private:
         enum class state_t {IDLE,SIGNAL};
         state_t STATE = state_t::IDLE;
         int n = 0;
+		int startTimeUS = hwlib::now_us();
         for(;;){
             switch(STATE){
                 case state_t::IDLE:
@@ -23,7 +24,8 @@ private:
                         Decoder.PauseDetected(n);
                         STATE = state_t::SIGNAL;
                     }else{
-                        n+=50;
+                        n+= hwlib::now_us() - startTimeUS;
+						startTimeUS = hwlib::now_us();
                     }
                     break;
                 case state_t::SIGNAL:
@@ -31,6 +33,7 @@ private:
                     if(!sensor.GetIrSignal()){
                         STATE = state_t::IDLE;
                         n = 0;
+						startTimeUS = hwlib::now_us();
                     }
                     break;
             }
@@ -42,7 +45,7 @@ public:
         task(priority, taskName),
         sensor(ir),
         Decoder(msg),
-        ReceiveClock(this,50,"ReceiveClock")
+        ReceiveClock(this,100,"ReceiveClock")
     {}
 };
 
